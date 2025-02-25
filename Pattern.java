0ac50897573549ac2805fec0e1bd4e0dc6878b97
Snehal <snehalsingh353@gmail.com>
2025-02-25 23:50:06 +0530

    initial commit

--- Pattern.java
+++ Pattern.java
@@ -2,15 +2,14 @@ import java.io.*;
 
 class Pattern {
     public static void main(String[] args) throws IOException {
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // int n = Integer.parseInt(br.readLine());
+        // printing a lower triangle in higher to lower
         for (int i = 5; i > 0; i--) {
             for (int j = i; j > 0; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
+        // printing a upper triangle in lower to higher
         for (int i = 1; i < 6; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
