65bb74c46919f0740cb5e1927f3f5a0f7c76ceee
Snehal <snehalsingh353@gmail.com>
2025-03-04 23:18:01 +0530

    INItial commit

--- AgeCal.java
+++ AgeCal.java
@@ -1,18 +1,49 @@
 import java.util.Scanner;
 
 public class AgeCal {
-    static Scanner sc = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        System.out.println("Enter your age:");
-        int n = sc.nextInt();
-        ageCal(n);
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 
-    public static void ageCal(int n) {
-        if (n < 0) {
-            System.out.println("Invalid Input");
+    static Scanner scanner = new Scanner(System.in);
+
+    private static int daysInMonth(int month, int year) {
+
+        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (month == 2 && isLeapYear(year)) {
+            return 29; // Leap year February
         }
-        System.out.println("The person age is " + n + " years," + n * 12 + " months, " + n * 12 * 365 + " days.");
+        return days[month - 1];
+    }
+
+    public static void main(String[] args) {
+        // Taking user input for date of birth
+        System.out.print("Enter your birth year (YYYY): ");
+        int birthYear = scanner.nextInt();
+        System.out.print("Enter your birth month (MM): ");
+        int birthMonth = scanner.nextInt();
+        System.out.print("Enter your birth day (DD): ");
+        int birthDay = scanner.nextInt();
+
+        // Taking user input for current date
+        System.out.print("Enter current year (YYYY): ");
+        int Year = scanner.nextInt();
+        System.out.print("Enter current month (MM): ");
+        int Month = scanner.nextInt();
+        System.out.print("Enter current day (DD): ");
+        int Day = scanner.nextInt();
+
+        int ageYear = Year - birthYear;
+        if (Day < 0) {
+            Month--;
+            Day += daysInMonth(birthMonth, birthYear);
+        }
+        if (Month < 0) {
+            ageYear--;
+            Month += 12;
+        }
+        System.out.println("Your age is " + ageYear + " years, " + Month + " months," + Day +
+                " ,days");
+
     }
 }
--- Pattern.java
+++ Pattern.java
@@ -2,6 +2,14 @@ import java.io.*;
 
 class Pattern {
     public static void main(String[] args) throws IOException {
+
+        // printing a upper triangle in lower to higher
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         // printing a lower triangle in higher to lower
         for (int i = 5; i > 0; i--) {
             for (int j = i; j > 0; j--) {
@@ -10,12 +18,5 @@ class Pattern {
             System.out.println();
         }
 
-        // printing a upper triangle in lower to higher
-        for (int i = 1; i < 6; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
 }
