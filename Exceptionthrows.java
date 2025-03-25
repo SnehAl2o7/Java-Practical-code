11bf2748cb6d4960cc958eba2eb1bc8e03070e84
SnehAl207 <snehalsingh353@gmail.com>
2025-03-25 15:31:24 +0530

    update

--- Exceptionthrows.java
+++ Exceptionthrows.java
@@ -1,11 +1,15 @@
+import java.util.*;
 public class Exceptionthrows {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         try{
-            int a = args.length;
+            int a = sc.nextInt();
             int b = 10/a;
+            System.out.println("Result is "+b);
             try{
                 int [] arr = {0};
                 int c = arr[5];
+                System.out.println("Result is "+c);
             }
             catch(ArrayIndexOutOfBoundsException e){
                 System.out.println("ArrayIndexOutOfBoundsException");
---
+++ Exceptionusingthrows.java
@@ -0,0 +1,5 @@
+public class Exceptionusingthrows {
+    public static void main(String [] args){
+        
+    }
+}
