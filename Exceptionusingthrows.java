e1e23493c255293357dc909b4380e8262f58b62a
SnehAl207 <snehalsingh353@gmail.com>
2025-03-25 16:12:22 +0530

    using throws in methods

--- Exceptionusingthrows.java
+++ Exceptionusingthrows.java
@@ -1,5 +1,33 @@
+import java.util.*;
 public class Exceptionusingthrows {
+    
+    public static int onlypositive(int a, int b)  throws Negative{
+        if(a<0 || b<0){
+            throw new Negative("no less than zero");
+        }
+
+        return a+b;
+    }
     public static void main(String [] args){
-        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        try{
+            System.out.println(onlypositive(a, b));    
+        }catch(Negative n){
+            System.out.println("the error is as follows: "+n);
+        }
+        finally{
+            System.out.println("this is finally block");
+        }
+
+        sc.close();
+    }
+}
+
+class Negative extends Exception{
+    
+    public Negative(String message){
+        super(message);    
     }
 }
